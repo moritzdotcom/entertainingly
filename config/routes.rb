92bddf9c4b504216c28bbeb6fa4a890960c66909
Moritz Löchner <momo.loechner@gmail.com>
2020-03-26 00:46:02 +0100

    npm install

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
 
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get '/about', to: 'pages#about_us', as: :about
 
   resources :categories, only: [:index, :show]
